49bd9bd32201f5776309bee9aeee1896c0b4646d
Manuja DeSilva <mdesilva@bu.edu>
2022-10-07 20:51:57 -0400

    generate two step verification url from failed sign in attempt, and then provide to user to complete sign in on browser

--- lib/FlexUnlimited.py
+++ lib/FlexUnlimited.py
@@ -117,12 +117,22 @@ class FlexUnlimited:
       "requested_token_type": ["bearer", "mac_dms", "website_cookies"]
     }
     try:
-      response = requests.post(FlexUnlimited.routes.get("GetAuthToken"),
+      response: Response = requests.post(FlexUnlimited.routes.get("GetAuthToken"),
                                headers=FlexUnlimited.allHeaders.get("AmazonApiRequest"), json=payload).json()
       return response.get("response").get("success").get("tokens").get("bearer").get("access_token")
     except Exception as e:
-      Log.error("Unable to authenticate to Amazon Flex. Please provide a valid Amazon Flex username and password.")
+      twoStepVerificationChallengeUrl = self.__getTwoStepVerificationChallengeUrl(response)
+      print("Unable to authenticate to Amazon Flex.")
+      print(f"\nPlease try completing the two step verification challenge at \033[1m{twoStepVerificationChallengeUrl}\033[0m . Then try again.")
+      print("\nIf you already completed the two step verification, please check your Amazon Flex username and password in the config file and try again.")
       sys.exit()
+
+  """
+  Parse the verification challenge code unique to the user from the failed login attempt and return the url where they can complete the two step verification.
+  """
+  def __getTwoStepVerificationChallengeUrl(self, challengeRequest: Response) -> str:
+    verificationChallengeCode: str = challengeRequest.get("response").get("challenge").get("uri").split("?")[1].split("=")[1]
+    return "https://www.amazon.com/ap/challenge?openid.return_to=https://www.amazon.com/ap/maplanding&openid.oa2.code_challenge_method=S256&openid.assoc_handle=amzn_device_ios_us&pageId=amzn_device_ios_light&accountStatusPolicy=P1&openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select&openid.mode=checkid_setup&openid.identity=http://specs.openid.net/auth/2.0/identifier_select&openid.ns.oa2=http://www.amazon.com/ap/ext/oauth/2&openid.oa2.client_id=device:30324244334531423246314134354635394236443142424234413744443936452341334e5748585451344542435a53&language=en_US&openid.ns.pape=http://specs.openid.net/extensions/pape/1.0&openid.oa2.code_challenge=n76GtDRiGSvq-Bhrez9x0CypsZFB_7eLfEDy_qZtqFk&openid.oa2.scope=device_auth_access&openid.ns=http://specs.openid.net/auth/2.0&openid.pape.max_auth_age=0&openid.oa2.response_type=code" + f"&arb={verificationChallengeCode}"
 
   @staticmethod
   def __getAmzDate() -> str:
