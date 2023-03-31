710137c80bbeebac07034e313ce0e1dbf3e4f0f6
possiblemeatball <mwalker@possiblemeatball.net>
2023-03-31 14:25:19 -0400

    add startTime from offerResponse

--- lib/Offer.py
+++ lib/Offer.py
@@ -8,28 +8,29 @@ class Offer:
     def __init__(self, offerResponseObject: object) -> None:
         self.id = offerResponseObject.get("offerId")
         self.expirationDate = datetime.fromtimestamp(offerResponseObject.get("expirationDate"))
+        self.startTime = datetime.fromtimestamp(offerResponseObject.get("startTime"))
         self.location = offerResponseObject.get('serviceAreaId')
         self.blockRate = float(offerResponseObject.get('rateInfo').get('priceAmount'))
         self.endTime = datetime.fromtimestamp(offerResponseObject.get('endTime'))
         self.hidden = offerResponseObject.get("hidden")
-        self.ratePerHour = self.blockRate / ((self.endTime - self.expirationDate).seconds / 3600)
+        self.ratePerHour = self.blockRate / ((self.endTime - self.startTime).seconds / 3600)
         self.weekday = self.expirationDate.weekday()
     
     def toString(self) -> str:
-        blockDuration = (self.endTime - self.expirationDate).seconds / 3600
+        blockDuration = (self.endTime - self.startTime).seconds / 3600
 
         body = 'Location: ' + self.location + '\n'
-        body += 'Date: ' + str(self.expirationDate.month) + '/' + str(self.expirationDate.day) + '\n'
+        body += 'Date: ' + str(self.startTime.month) + '/' + str(self.startTime.day) + '\n'
         body += 'Pay: ' + str(self.blockRate) + '\n'
         body += 'Pay rate per hour: ' + str(self.ratePerHour) + '\n'
         body += 'Block Duration: ' + str(blockDuration) + f'{"hour" if blockDuration == 1 else "hours"}\n'
 
-        if not self.expirationDate.minute:
-            body += 'Start time: ' + str(self.expirationDate.hour) + '00\n'
-        elif self.expirationDate.minute < 10:
-            body += 'Start time: ' + str(self.expirationDate.hour) + '0' + str(self.expirationDate.minute) + '\n'
+        if not self.startTime.minute:
+            body += 'Start time: ' + str(self.startTime.hour) + '00\n'
+        elif self.startTime.minute < 10:
+            body += 'Start time: ' + str(self.startTime.hour) + '0' + str(self.startTime.minute) + '\n'
         else:
-            body += 'Start time: ' + str(self.expirationDate.hour) + str(self.expirationDate.minute) + '\n'
+            body += 'Start time: ' + str(self.startTime.hour) + str(self.startTime.minute) + '\n'
 
         if not self.endTime.minute:
             body += 'End time: ' + str(self.endTime.hour) + '00\n'
